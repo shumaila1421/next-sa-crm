ac550084bd963ee53eabf851d6544de9dbb4610a
Shumaila Riaz <shumailariaz215@gmail.com>
2026-08-27 19:51:26 +0500

    create aaplication table

---
+++ client/components/ApplicationTable.tsx
@@ -0,0 +1,34 @@
+import type { Application } from "@/types/application";
+import { table } from "console";
+
+type ApplicationTableProps = {
+  applications: Application[];
+};
+
+export default function ApplicationTable({
+  applications,
+}: ApplicationTableProps) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Student</th>
+          <th>University</th>
+          <th>Program</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {applications.map((application) => (
+          <tr key={application.student}>
+            <td>{application.student}</td>
+            <td>{application.university}</td>
+            <td>{application.program}</td>
+            <td>{application.status}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
